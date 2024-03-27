9a5057dd9ff84b1e24277a13185e1086f6814262
STRIKEox <165011411+STRIKEox@users.noreply.github.com>
2024-03-27 15:22:10 +0530

    main.py

--- main.py
+++ main.py
@@ -17,22 +17,22 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def load_plugins(plugin_name):
     path = Path(f"AltBots/modules/{plugin_name}.py")
-    spec = importlib.util.spec_from_file_location(f"AltBots.modules.{plugin_name}", path)
+    spec = importlib.util.spec_from_file_location(f"FinalStrike.modules.{plugin_name}", path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["AltBots.modules." + plugin_name] = load
-    print("Altron has Imported " + plugin_name)
+    sys.modules["FinalStrike.modules." + plugin_name] = load
+    print("Final Strike Has uploaded " + plugin_name)
 
 
-files = glob.glob("AltBots/modules/*.py")
+files = glob.glob("FinalStrike/modules/*.py")
 for name in files:
     with open(name) as a:
         patt = Path(a.name)
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\n𝐗𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 ⚡\nMy Master ---> @𝐏𝐲𝐗𝐞𝐧")
+print("\n𝐗𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 ⚡\nMy Master ---> @STRIKE_OX")
 
 
 async def main():
